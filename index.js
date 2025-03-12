4cd2c68d0afbd8939527882ebd0a1e9604ab27a9
bahlool1401 <bahlool.ramesh@gmail.com>
2025-03-12 23:34:08 +0330

    add some orders for Robot

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const {Telegraf} = require('telegraf');
 
 const bot = new Telegraf('7878032273:AAEogWl6kdsxkDarCfu-E81maK_jKMnx3RY')
+
+bot.start((ctx)=>{
+    ctx.reply('HI HONEY😍.WELCOME TO THE CHANELL🌹 \n our services include : **** \n /help ')
+})
+
+bot.help((ctx)=>{
+    ctx.reply('our socials are \n Telegram \n Youtube \n 😍😎')
+})
+
+bot.hears('Youtube',(ctx)=>{
+    ctx.reply('https://www.youtube.com')
+})
+bot.hears('shahr',(ctx)=>{
+    ctx.reply('https://www.shahrekhabar.com')
+})
+
+bot.launch()
