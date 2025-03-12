0c777fd0d26ab690ae0536aaf2ed8cf93242eec8
bahlool1401 <bahlool.ramesh@gmail.com>
2025-03-12 23:57:59 +0330

    add two commands (sum(Addition of numbers)) + write and say back

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-const {Telegraf} = require('telegraf');
+const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf('7878032273:AAEogWl6kdsxkDarCfu-E81maK_jKMnx3RY')
+const bot = new Telegraf("7878032273:AAEogWl6kdsxkDarCfu-E81maK_jKMnx3RY");
 
-bot.start((ctx)=>{
-    ctx.reply('HI HONEY😍.WELCOME TO THE CHANELL🌹 \n our services include : **** \n /help ')
+bot.start((ctx) => {
+  ctx.reply(
+    "HI HONEY😍.WELCOME TO THE CHANELL🌹 \n our services include : **** \n /help "
+  );
+});
+
+bot.help((ctx) => {
+  ctx.reply("our socials are \n Telegram \n Youtube \n 😍😎");
+});
+
+bot.hears("Youtube", (ctx) => {
+  ctx.reply("https://www.youtube.com");
+});
+bot.hears("shahr", (ctx) => {
+  ctx.reply("https://www.shahrekhabar.com");
+});
+
+//COMMAND************
+bot.command('say' , (ctx) => {
+  msg = ctx.message.text;
+  msgArray = msg.split(" ");
+
+  if (msgArray.length < 2) {
+    ctx.reply("write what i say.");
+  } else {
+    msgArray.shift();
+    newMsg = msgArray.join(" ");
+    ctx.reply(newMsg);
+  }
+});
+
+bot.command('sum',(ctx)=>{
+    msg = ctx.message.text
+    msgArray = msg.split(' ')
+    if(msgArray<2){
+        ctx.reply('enter numbers with spaces.')
+    }else{
+        msgArray.shift()
+        sum = 0 
+        for(i=0 ; i<msgArray.length;i++){
+            sum = parseInt(msgArray(i))+parseInt(sum)
+        }
+        ctx.reply(sum.toString())
+    }    
 })
 
-bot.help((ctx)=>{
-    ctx.reply('our socials are \n Telegram \n Youtube \n 😍😎')
-})
-
-bot.hears('Youtube',(ctx)=>{
-    ctx.reply('https://www.youtube.com')
-})
-bot.hears('shahr',(ctx)=>{
-    ctx.reply('https://www.shahrekhabar.com')
-})
-
-bot.launch()
+bot.launch();
